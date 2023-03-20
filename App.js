21175b8db513a4ba2ca34da6bac3aa343a84dbc4
dario-nihil <dario.nihil@gmail.com>
2023-03-20 09:29:09 +0100

    started implementing layout

--- App.js
+++ App.js
@@ -1,18 +1,21 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, TextInput, View } from "react-native";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Hello World!</Text>
+    <View style={styles.appContainer}>
+      <View>
+        <TextInput placeholder="Your course goal!" />
+        <Button title="Add Goal" />
+      </View>
+      <View>
+        <Text>List of goals...</Text>
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  appContainer: {
+    padding: 50,
   },
 });
