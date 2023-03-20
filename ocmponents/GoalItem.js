c44373448c44adc383d46429bf162663a0e37609
dario-nihil <dario.nihil@gmail.com>
2023-03-20 15:13:35 +0100

    add style feedback on goal deletion

--- ocmponents/GoalItem.js
+++ ocmponents/GoalItem.js
@@ -2,11 +2,15 @@ import { StyleSheet, View, Text, Pressable } from "react-native";
 
 const GoalItem = ({ id, text, onDeleteItem }) => {
   return (
-    <Pressable onPress={onDeleteItem.bind(null, id)}>
-      <View style={styles.goalItem}>
+    <View style={styles.goalItem}>
+      <Pressable
+        android_ripple={{ color: "#15022d" }}
+        onPress={onDeleteItem.bind(null, id)}
+        style={({ pressed }) => pressed && styles.pressedItem}
+      >
         <Text style={styles.goalText}>{text}</Text>
-      </View>
-    </Pressable>
+      </Pressable>
+    </View>
   );
 };
 
@@ -15,11 +19,14 @@ export default GoalItem;
 const styles = StyleSheet.create({
   goalItem: {
     margin: 8,
-    padding: 8,
     borderRadius: 6,
     backgroundColor: "#5e0acc",
   },
+  pressedItem: {
+    opacity: 0.5,
+  },
   goalText: {
     color: "#ffffff",
+    padding: 8,
   },
 });
